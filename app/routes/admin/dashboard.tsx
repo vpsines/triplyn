0542db7911a4d678d698f2622b68a8189ce80970
vpsines <sasip6028@gmail.com>
2025-11-15 11:04:27 +0530

    feat:add dashboard and all-users route

---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className="text-black">Dashboard assssssssssssssssssssssssssss</div>
+    )
+}
+export default Dashboard
